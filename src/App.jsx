eca5387c5326d165427b223bc3bc1606f5686fd5
Aaron Belmore <belmoreaaron9@gmail.com>
2024-02-01 01:17:03 -0500

    quiz app uplaod

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu";
+import { gameStateContext } from "./helpers/Contexts";
 
 function App() {
   const [gameState, setGameState] = useState("menu");
+
   return (
     <div className="main_container">
       <h1>Quiz App</h1>
-      {gameState === "menu" && <Menu />}
+      <gameStateContext.Provider value={{ gameState, setGameState }}>
+        {gameState === "menu" && <Menu />}
+      </gameStateContext.Provider>
     </div>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { gameStateContext } from "../helpers/Contexts";
 import "../App.css";
 
 // ['menu','playing','finished']
 const Menu = () => {
-
+  const { gameState, setGameState } = useContext(gameStateContext);
   return (
     <div className="menu_container">
       <label htmlFor="menu_input">Enter Your Name:</label>
       <input type="text" placeholder="Ex.How many" id="menu_input" />
-      <button>Start Quiz</button>
+      <button onClick={() => setGameState("playing")}>Start Quiz</button>
     </div>
   );
 };
